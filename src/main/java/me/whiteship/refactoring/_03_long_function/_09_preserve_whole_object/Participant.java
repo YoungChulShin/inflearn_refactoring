8920449a77bd45f3663c887bbdb5e6740950e92a
YoungChulShin <go1323@gmail.com>
2022-04-04 19:29:54 +0900

    긴 함수 - 객체를 통체로 넘기기
    - 메서드의 파라미터가 특정 객체로 부터 가져오는 값들이라면 객체를 통으로 넘기는 것을 검토한다
    - 이때 객체의 전체 의존성을 넘기는 것이 맞는지를 고민해야한다

--- src/main/java/me/whiteship/refactoring/_03_long_function/_09_preserve_whole_object/Participant.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_09_preserve_whole_object/Participant.java
@@ -4,13 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public record Participant(String username, Map<Integer, Boolean> homework) {
-    public Participant(String username) {
-        this(username, new HashMap<>());
-    }
 
-    public void setHomeworkDone(int index) {
-        this.homework.put(index, true);
-    }
+  public Participant(String username) {
+    this(username, new HashMap<>());
+  }
 
+  public void setHomeworkDone(int index) {
+    this.homework.put(index, true);
+  }
 
+  double getRate(int totalNumberOfEvents) {
+    long count = homework().values().stream()
+        .filter(v -> v == true)
+        .count();
+    return (double) (count * 100 / totalNumberOfEvents);
+  }
 }
