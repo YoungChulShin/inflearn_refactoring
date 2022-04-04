d63473ccc4a558a1dae5b5a9e5a1c75661097501
YoungChulShin <go1323@gmail.com>
2022-04-04 16:42:26 +0900

    중복 코드 - 메서드 올리기

--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/Dashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/Dashboard.java
@@ -1,6 +1,11 @@
 package me.whiteship.refactoring._02_duplicated_code._06_pull_up_method;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 
 public class Dashboard {
 
@@ -10,5 +15,16 @@ public class Dashboard {
 
         ParticipantDashboard participantDashboard = new ParticipantDashboard();
         participantDashboard.printParticipants(15);
+    }
+
+    protected void printUserNames(int eventId) throws IOException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(30);
+
+        Set<String> userNames = new HashSet<>();
+        issue.getComments().forEach(c -> userNames.add(c.getUserName()));
+
+        userNames.forEach(System.out::println);
     }
 }
--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/ReviewerDashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/ReviewerDashboard.java
@@ -1,27 +1,11 @@
 package me.whiteship.refactoring._02_duplicated_code._06_pull_up_method;
 
-import org.kohsuke.github.GHIssue;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
-
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
 
 public class ReviewerDashboard extends Dashboard {
 
     public void printReviewers() throws IOException {
-        // Get github issue to check homework
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(30);
-
-        // Get reviewers
-        Set<String> reviewers = new HashSet<>();
-        issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
-
-        // Print reviewers
-        reviewers.forEach(System.out::println);
+        printUserNames(30);
     }
 
 }
