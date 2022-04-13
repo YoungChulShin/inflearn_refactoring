4f50be75f63463a2295f2ef0790f616192e0b0ea
YoungChulShin <go1323@gmail.com>
2022-04-13 19:40:12 +0900

    뒤엉킨 변경 - 단계 쪼개기
    - 메서드 내에서 서로 다른 일을 하는게 있다면 각가의 함수로 분리한다
    - 이 과정에서 중간 계산을 정보를 저장하는 클래스가 추가될 수 있다

--- inflearn_refactoring/src/main/java/me/whiteship/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
+++ inflearn_refactoring/src/main/java/me/whiteship/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
@@ -3,13 +3,20 @@ package me.whiteship.refactoring._07_divergent_change._24_split_phase;
 public class PriceOrder {
 
     public double priceOrder(Product product, int quantity, ShippingMethod shippingMethod) {
+        PriceData priceData = calculatePriceData(product, quantity);
+        return applyShippingCost(priceData, shippingMethod);
+    }
+
+    private PriceData calculatePriceData(Product product, int quantity) {
         final double basePrice = product.basePrice() * quantity;
-        final double discount = Math.max(quantity - product.discountThreshold(), 0)
-                * product.basePrice() * product.discountRate();
-        final double shippingPerCase = (basePrice > shippingMethod.discountThreshold()) ?
+        final double discount = Math.max(quantity - product.discountThreshold(), 0) * product.basePrice() * product.discountRate();
+        return new PriceData(basePrice, discount, quantity);
+    }
+
+    private double applyShippingCost(PriceData priceData, ShippingMethod shippingMethod) {
+        final double shippingPerCase = (priceData.basePrice() > shippingMethod.discountThreshold()) ?
                 shippingMethod.discountedFee() : shippingMethod.feePerCase();
-        final double shippingCost = quantity * shippingPerCase;
-        final double price = basePrice - discount + shippingCost;
-        return price;
+        final double shippingCost = priceData.quantity() * shippingPerCase;
+        return priceData.basePrice() - priceData.discount() + shippingCost;
     }
 }
