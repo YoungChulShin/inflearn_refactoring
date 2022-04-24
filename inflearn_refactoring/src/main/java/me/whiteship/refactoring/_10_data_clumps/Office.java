faef96ecc8e7d679d79bd3c5ae131e465f0f2fa1
YoungChulShin <go1323@gmail.com>
2022-04-25 06:41:27 +0900

    코드 뭉치
    - 연관 데이터 묶음을 별도의 클래스로 분리

--- inflearn_refactoring/src/main/java/me/whiteship/refactoring/_10_data_clumps/Office.java
+++ inflearn_refactoring/src/main/java/me/whiteship/refactoring/_10_data_clumps/Office.java
@@ -4,33 +4,10 @@ public class Office {
 
     private String location;
 
-    private String officeAreCode;
+    private TelephoneNumber telephoneNumber;
 
-    private String officeNumber;
-
-    public Office(String location, String officeAreCode, String officeNumber) {
+    public Office(String location, TelephoneNumber telephoneNumber) {
         this.location = location;
-        this.officeAreCode = officeAreCode;
-        this.officeNumber = officeNumber;
-    }
-
-    public String officePhoneNumber() {
-        return officeAreCode + "-" + officeNumber;
-    }
-
-    public String getOfficeAreCode() {
-        return officeAreCode;
-    }
-
-    public void setOfficeAreCode(String officeAreCode) {
-        this.officeAreCode = officeAreCode;
-    }
-
-    public String getOfficeNumber() {
-        return officeNumber;
-    }
-
-    public void setOfficeNumber(String officeNumber) {
-        this.officeNumber = officeNumber;
+        this.telephoneNumber = telephoneNumber;
     }
 }
