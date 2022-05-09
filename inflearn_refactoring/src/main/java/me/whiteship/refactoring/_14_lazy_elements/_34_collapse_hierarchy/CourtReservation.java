c8b34a40e8c4ba4717359e7155fc16b97ed9ace3
YoungChulShin <go1323@gmail.com>
2022-05-10 06:57:53 +0900

    collapse hierachy
    - 상속 구조를 리팩토링할 때, 둘의 기능에 차이가 없어지면 합칠 수 있다

--- inflearn_refactoring/src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/CourtReservation.java
+++ inflearn_refactoring/src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/CourtReservation.java
@@ -1,7 +1,9 @@
 package me.whiteship.refactoring._14_lazy_elements._34_collapse_hierarchy;
 
+import java.time.LocalDateTime;
+
 public class CourtReservation extends Reservation {
 
-    private String courtNumber;
-
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
 }
