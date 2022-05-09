7e83d728852518b090121d4197f1ea15b4d11529
YoungChulShin <go1323@gmail.com>
2022-05-10 06:37:11 +0900

    반복되는 switch문

--- inflearn_refactoring/src/test/java/me/whiteship/refactoring/_11_primitive_obsession/_31_replace_type_code_with_subclasses/indirect_inheritance/EmployeeTest.java
+++ inflearn_refactoring/src/test/java/me/whiteship/refactoring/_11_primitive_obsession/_31_replace_type_code_with_subclasses/indirect_inheritance/EmployeeTest.java
@@ -1,5 +1,8 @@
 package me.whiteship.refactoring._11_primitive_obsession._31_replace_type_code_with_subclasses.indirect_inheritance;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +11,11 @@ class EmployeeTest {
 
     @Test
     void capitalizedType() {
+
+        List<String> strings = new ArrayList<>(Arrays.asList("1", "2", "3"));
+        strings.add("1");
+
+
         assertEquals("Manager", new FullTimeEmployee("keesun", "manager").capitalizedType());
         assertEquals("Engineer", new PartTimeEmployee("keesun", "engineer").capitalizedType());
         assertThrows(IllegalArgumentException.class, () -> new Employee("keesun", "wrong type"));
