3160809e3fb7db5b163774978948fcb180fc57e6
YoungChulShin <go1323@gmail.com>
2022-05-13 06:40:29 +0900

    서로 다른 인터페이스의 대안 클래스들
    - 서로 다른 인터페이스를 감싸는 상위 추상화계층을 만들어서 추상화한다

---
+++ inflearn_refactoring/src/main/java/me/whiteship/refactoring/_21_alternative_classes_with_different_interfaces/EmailNotificationService.java
@@ -0,0 +1,19 @@
+package me.whiteship.refactoring._21_alternative_classes_with_different_interfaces;
+
+public class EmailNotificationService implements NotificationService {
+
+  private EmailService emailService;
+
+  public EmailNotificationService(EmailService emailService) {
+    this.emailService = emailService;
+  }
+
+  @Override
+  public void sendNotification(Notification notification) {
+    EmailMessage emailMessage = new EmailMessage();
+    emailMessage.setTitle(notification.getTitle());
+    emailMessage.setTo(notification.getSender());
+    emailMessage.setFrom(notification.getReceiver());
+    emailService.sendEmail(emailMessage);
+  }
+}
