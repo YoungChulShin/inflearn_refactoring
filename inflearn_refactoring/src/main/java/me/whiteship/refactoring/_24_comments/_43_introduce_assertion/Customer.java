54bffa6b2e2fcfc7c6b533a6293c529a7a84b512
YoungChulShin <go1323@gmail.com>
2022-05-13 07:10:02 +0900

    어서션 추가하기
    - runtime에서는 assert 동작하지 않는다. 테스트코드는 동작
    - 커뮤니케이션의 목적

--- inflearn_refactoring/src/main/java/me/whiteship/refactoring/_24_comments/_43_introduce_assertion/Customer.java
+++ inflearn_refactoring/src/main/java/me/whiteship/refactoring/_24_comments/_43_introduce_assertion/Customer.java
@@ -13,6 +13,7 @@ public class Customer {
     }
 
     public void setDiscountRate(Double discountRate) {
+        assert discountRate != null && discountRate > 0;    // 커뮤니케이션 목적
         this.discountRate = discountRate;
     }
 }
